8d4ae31a2596c61d2380541cd5535ed4b21b0d02
Shant Dashjian <shantgdashjian@gmail.com>
2025-03-27 22:18:38 +0400

    Add manager skeleton

--- manager/manager.go
+++ manager/manager.go
@@ -1,1 +1,30 @@
 package manager
+
+import (
+	"cube/task"
+	"fmt"
+
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+)
+
+type Manager struct {
+	Pending			queue.Queue
+	TaskDb 			map[uuid.UUID]*task.Task
+	EventDb 		map[uuid.UUID]*task.TaskEvent
+	Workers			[]string
+	WorkerTaskMap	map[string][]uuid.UUID
+	TasWorkerkMap	map[uuid.UUID]string
+}
+
+func (m *Manager) SelectWorker() {
+	fmt.Println("I will select an appropriate worker")
+}
+
+func (m *Manager) UpdateTasks() {
+	fmt.Println("I will update tasks")
+}
+
+func (m *Manager) SendWork() {
+	fmt.Println("I will send work to workers")
+}
