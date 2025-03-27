ed659358b1b3cbe897706d0f8bdff52121fb9f3b
Shant Dashjian <shantgdashjian@gmail.com>
2025-03-27 16:23:05 +0400

    Add task skeleton

---
+++ task/task.go
@@ -0,0 +1,38 @@
+package task
+
+import (
+	"github.com/google/uuid"
+	"github.com/docker/go-connections/nat"
+)
+
+
+type State int
+
+const (
+	Pending State = iota
+	Scheduled
+	Running
+	Completed
+	Failed
+)
+
+type Task struct (
+	ID				uuid.UUID
+	Name			string
+	State			State
+	Image			string
+	Memory			int
+	Disk			int
+	ExposedPorts	nat.PortSet
+	PortBindings	map[string]string
+	RestartPolicy	string	
+	StartTime		time.Time
+	FinishTime		time.Time
+)
+
+type Task struct (
+	ID			uuid.UUID
+	State		State
+	Timestamp	time.Time
+	Task		Task
+)
